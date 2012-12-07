44637f141c87f9bc2b4af8ee65afeb914eef2262
Adam Mesha <adam@mesha.org>
2012-12-07 06:32:07 +0200

    basic admin

---
+++ alighi/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from alighi.models import *
+
+admin.site.register(Respondeco)
+admin.site.register(Valuto)
+admin.site.register(Kurzo)
+admin.site.register(AghKategorio)
+admin.site.register(AlighKategorio)
+admin.site.register(LandoKategorio)
+admin.site.register(Lando)
+admin.site.register(LoghKategorio)
+admin.site.register(ManghoMendo)
+admin.site.register(ManghoTipo)
+admin.site.register(Kotizero)
+admin.site.register(Pagmaniero)
+admin.site.register(Retposhtajho)
+admin.site.register(SurlokMembrighaKategorio)
+admin.site.register(Chambro) # unuopa chambro por disdoni
+admin.site.register(Partoprenanto)
+admin.site.register(Pago)
+admin.site.register(MinimumaAntaupago)
+admin.site.register(Nomshildo)
+admin.site.register(Noto)
