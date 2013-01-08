f4905b3a1123628a8b2a78608dddd37124eaae04
Adam Mesha <adam@mesha.org>
2013-01-08 10:57:22 +0100

    test data update

--- scripts/dumpdata.sh
+++ scripts/dumpdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python manage.py dumpdata --indent=4 --format=json alighi auth admin flatpages sites > data.json
+./manage.py dumpdata --indent=4 --format=json alighi auth admin flatpages sites > data.json
 
