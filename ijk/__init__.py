462d74c8e4afda32a6c2522570f213665fda10ce
Adam Mesha <adam@mesha.org>
2013-01-27 04:23:14 +0200

    menu based on mptt

--- ijk/__init__.py
+++ ijk/__init__.py
@@ -36,7 +36,7 @@ show_in_menu = fields.BooleanField(u'Ĉu montri en menuo',
 show_in_menu.contribute_to_class(FlatPage, u'show_in_menu')
 
 # create a key for ordering the menu
-sort_key = fields.IntegerField(u'Ordiga sxlosilo',
+sort_key = fields.IntegerField(u'Ordiga ŝlosilo',
     help_text=u'Ŝlosilo por ordigi menuerojn kun sama patro '
     u'unu rilate al la alia', blank=True, null=True)
 sort_key.contribute_to_class(FlatPage, u'sort_key')
@@ -52,6 +52,11 @@ TreeForeignKey(FlatPage, verbose_name='patro',
                blank=True, null=True).contribute_to_class(FlatPage, 'parent')
 
 mptt.register(FlatPage, order_insertion_by=[u'sort_key', u'url'])
+
+# For some reason, FlatPage.tree is an ordinary manager, not a TreeManager
+treemanager = mptt.managers.TreeManager()
+treemanager._base_manager = None
+treemanager.contribute_to_class(FlatPage, 'treemanager')
 
 class NewFlatpageForm(flatforms.FlatpageForm, MPTTAdminForm):
     sites = ModelMultipleChoiceField(queryset=Site.objects.all(),
@@ -70,54 +75,25 @@ class NewFlatPageAdmin(flatadmin.FlatPageAdmin,
     )
 
 admin.site.unregister(FlatPage)
-#admin.site.register(FlatPage, NewFlatPageAdmin, Media=EditorMedia)
 admin.site.register(FlatPage, NewFlatPageAdmin, Media=EditorMedia)
 
+from django.core.urlresolvers import reverse
 
+_roots = FlatPage.treemanager.root_nodes()
 
-class Menu(object):
-    def __init__(self):
-        self.menu = OrderedDict() # mapping from parent url to Menu object
-        self.mainpage = None
-    @staticmethod
-    def get_levels(url):
-        if url == u'/':
-            return []
-        else:
-            return url.strip('/').split('/')
-    @staticmethod
-    def get_label(url, title):
-        return u'<a href="{}">{}</a>'.format(url, title)
-    def addfp(self, item):
-        self.add(item.url, item.title)
-    def add(self, url, title):
-        label = self.get_label(url, title)
-        levels = self.get_levels(url)
-        self.additem(label, levels)
-    def additem(self, label, levels=None):
-        if not levels:
-            self.mainpage = label
-        else:
-            if levels[0] not in self.menu:
-                self.menu[levels[0]] = Menu()
-            self.menu[levels[0]].additem(label, levels[1:])
-    def to_html(self, toplevel=True):
-        label = self.mainpage if self.mainpage is not None else u''
-        items = [self.menu[key].to_html(toplevel=False)
-                    for key in self.menu]
-        if toplevel:
-            items = [label] + items
-            label = u''
-        if not items:
-            return label
-        items = u'<ul>{}</ul>'.format(u''.join(u'<li>{}</li>'.format(item)
-                                               for item in items))
-        return label + items
+def _mkmenu(node):
+    '''Return a <li> that links to the item at node and
+    includes a submenu of all its descendants'''
+    sublist = [_mkmenu(child) for child in node.get_children()]
+    return u'''<li><a href="{}">{}</a>{}</li>'''.format(
+        node.get_absolute_url(),
+        node.menu_title.strip() or node.title,
+        u'<ul>{}</ul>'.format(u''.join(sublist)) if sublist else u'')
 
-menu = Menu()
-for fp in FlatPage.objects.order_by('pk'):
-    menu.addfp(fp)
-menu.add(u'/alighi/', u'Aliĝi!')
-menu_html = menu.to_html()
+_menu = [_mkmenu(node) for node in _roots]
+_menu.append(u'<li><a href="{}">Aliĝi!</a></li>'.format(reverse('alighi')))
+menu_html = u'<ul>{}</ul>'.format(u''.join(_menu))
+
 def menu_context_processor(request):
     return {'MENUO': mark_safe(menu_html)}
+
