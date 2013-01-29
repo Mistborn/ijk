35025ff1b936dda0989d57a4c79ee70a9ccc2e26
Adam Mesha <adam@mesha.org>
2013-01-29 23:25:03 +0200

    add static media file serving in debug mode

--- ijk/urls.py
+++ ijk/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 if settings.DEBUG:
+    from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
@@ -24,4 +25,7 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns = (
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
+        staticfiles_urlpatterns() +
+        urlpatterns)
