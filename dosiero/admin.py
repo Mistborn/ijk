42ba77d6f5ad07bab0d5b15364e91bd8dc710750
Adam Mesha <adam@mesha.org>
2013-01-30 17:45:11 +0200

    mostly admin changes for the dosiero app

--- dosiero/admin.py
+++ dosiero/admin.py
@@ -1,5 +1,20 @@
+# -*- encoding: utf-8 -*-
 from django.contrib import admin
-
 from dosiero.models import *
 
-admin.site.register(Dosiero)
+class DosieroAdmin(admin.ModelAdmin):
+    def ligilo(self, dosiero):
+        return mark_safe(u'<a href="{url}">{url}</a>'.format(url=dosiero.url))
+    ligilo.allow_tags = True
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj is None:
+            return ()
+        return ('dosiero', 'ligilo',)
+
+    def get_list_display(self, request):
+        return ('priskribo', 'ligilo',)
+        
+    list_fields = ('priskribo', 'dosiero', 'ligilo',)
+    
+admin.site.register(Dosiero, DosieroAdmin)
--- dosiero/models.py
+++ dosiero/models.py
@@ -1,13 +1,24 @@
+# -*- encoding: utf-8 -*-
 from django.db import models
+from django.utils.safestring import mark_safe
 
 class Dosiero(models.Model):
-    priskribo = models.CharField(max_length=250)
+    priskribo = models.CharField(max_length=250, blank=True,
+        help_text=u'Ricevos la nomon de la dosiero se vi lasos ĝin malplena.')
     dosiero = models.FileField(upload_to=u'dosiero')
     
     def set_priskribo(self):
         if self.priskribo:
             return
         self.priskribo = self.dosiero.name
+
+    @property
+    def url(self):
+        return self.dosiero.url
+
+    def save(self):
+        self.set_priskribo()
+        super(Dosiero, self).save()
         
     def __unicode__(self):
         return self.priskribo
