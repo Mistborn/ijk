825cc7409938d5915afe9ab560601482d0741304
Adam Mesha <adam@mesha.org>
2013-02-05 10:52:47 +0200

    adding slashes to urls, and renaming /admin/ to /mastrumilo/

--- ijk/urls.py
+++ ijk/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.shortcuts import redirect
 
 if settings.DEBUG:
     from django.conf.urls.static import static
@@ -13,11 +14,18 @@ urlpatterns = patterns('',
     # url(r'^$', 'ijk.views.home', name='home'),
     # url(r'^ijk/', include('ijk.foo.urls')),
 
+    # all our urls end in slashes
+    url(r'^(?P<url>.*[^/])$',
+        lambda request, url: redirect('/'+url.strip('/')+'/')),
+    # actually we want to call the admin mastrumilo
+    url(r'^admin(?P<url>.*)$',
+        lambda request, url: redirect('/mastrumilo'+url)),
+
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^mastrumilo/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^mastrumilo/', include(admin.site.urls)),
 
     url(r'^alighi/', include('alighi.urls')),
     url('^', include('django.contrib.flatpages.urls')),
