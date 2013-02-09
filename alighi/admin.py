adcb46357d7939622f5a016f6bf693af64686572
Adam Mesha <adam@mesha.org>
2013-02-09 06:18:38 +0200

    sergey's changes

--- alighi/admin.py
+++ alighi/admin.py
@@ -38,7 +38,7 @@ class ProgramKotizoAdmin(reversion.VersionAdmin):
 class ProgramKotizoInline(admin.TabularInline,):
     model = ProgramKotizo
     extra = 0
-    
+
 class AghKategorioAdmin(reversion.VersionAdmin):
     list_display = ('nomo', 'priskribo', 'limagho', 'aldona_kotizo')
     list_editable = ('priskribo', 'limagho', 'aldona_kotizo')
@@ -67,12 +67,12 @@ class UEARabatoAdmin(reversion.VersionAdmin):
 class UEARabatoInline(admin.TabularInline):
     model = UEARabato
     extra = 0
-    
+
 class LandoKategorioAdmin(reversion.VersionAdmin):
     list_display = ('nomo', 'priskribo')
     list_editable = ('priskribo',)
     search_fields = ('nomo', 'priskribo')
-    inlines = (LandoInline, ProgramKotizoInline, UEARabatoInline)
+    inlines = (ProgramKotizoInline, UEARabatoInline, LandoInline,)
 
 class ChambroAdmin(reversion.VersionAdmin):
     list_display = ('nomo', 'litonombro', 'loghkategorio', 'rimarko')
@@ -82,7 +82,7 @@ class ChambroAdmin(reversion.VersionAdmin):
 class ChambroInline(admin.TabularInline):
     model = Chambro
     extra = 0
-    
+
 class LoghKategorioAdmin(reversion.VersionAdmin):
     list_display = ('nomo', 'priskribo', 'plena_kosto', 'unutaga_kosto')
     list_editable = list_display[1:]
@@ -107,7 +107,7 @@ class ManghoTipoAdmin(reversion.VersionAdmin):
     #list_display_links = ('model_unicode',)
     #list_editable = list_display[1:]
     pass
-    
+
 class PagmanieroAdmin(reversion.VersionAdmin):
     list_display = ('nomo', 'priskribo', 'komenta_etikedo',
                     'chu_publika', 'chu_nurisraela')
@@ -119,12 +119,12 @@ class RetposhtajhoAdmin(reversion.VersionAdmin):
     list_display = ('nomo', 'sendadreso', 'temo', 'teksto')
     list_editable = list_display[1:]
     list_filter = ('sendadreso',)
-    search_fields = ('nomo', 'temo', 'teksto')
+    search_fields = ('nomo', 'temo', 'teksto',)
 
 class SurlokaMembrighoAdmin(reversion.VersionAdmin):
     list_display = ('partoprenanto', 'kategorio', 'kotizo', 'valuto')
     #list_editable = list_display[1:]
-    list_filter = ('partoprenanto', 'kategorio', 'kotizo', 'valuto')
+    list_filter = ('kategorio', 'kotizo', 'valuto', 'partoprenanto',)
 class SurlokaMembrighoInline(admin.TabularInline):
     model = SurlokaMembrigho
     extra = 0
@@ -139,9 +139,8 @@ class PagoAdmin(reversion.VersionAdmin):
     readonly_fields = ('kreinto', 'lasta_redaktanto')
     list_editable = ('respondeculo', 'pagmaniero', 'pagtipo', 'valuto',
                      'rimarko',)
-    list_filter = ('partoprenanto', 'respondeculo', 'kreinto',
-                   'lasta_redaktanto', 'pagmaniero',
-                   'pagtipo', 'valuto', 'dato')
+    list_filter = ('partoprenanto', 'respondeculo', 'kreinto', 'lasta_redaktanto', 'pagmaniero',
+                   'pagtipo', 'valuto', 'dato',)
     search_fields = ('rimarko',)
     def save_model(self, request, obj, form, change):
         if not change and not obj.kreinto:
@@ -157,8 +156,8 @@ class NotoAdmin(reversion.VersionAdmin):
     list_display = ('partoprenanto', 'uzanto', 'dato', 'enhavo',
                     'chu_prilaborita', 'revidu')
     list_editable = ('uzanto', 'chu_prilaborita', 'revidu')
-    list_filter = ('partoprenanto', 'uzanto', 'dato', 'chu_prilaborita',
-                   'revidu')
+    list_filter = ('uzanto', 'dato', 'chu_prilaborita', 'revidu',
+                   'partoprenanto',)
     search_fields = ('enhavo',)
 class NotoInline(admin.TabularInline):
     model = Noto
@@ -189,8 +188,8 @@ class SenditaOficialajhoAdmin(reversion.VersionAdmin, OficialajhoAdminBase):
     search_fields = ('priskribo', 'dosiero__name')
     actions = None
     readonly_fields = ('alshutinto', 'alshutodato', 'elshuto',)
-    fields = ('priskribo', 'partoprenanto', 'dosiero',
-              'alshutinto', 'alshutodato', 'elshuto')
+    fields = ('priskribo', 'dosiero', 'alshutinto', 'alshutodato', 'elshuto',
+              'partoprenanto')
 class OficialajhoInline(admin.TabularInline, OficialajhoAdminBase):
     extra = 0
     can_delete = False
@@ -211,8 +210,8 @@ class SenditaRetposhtajhoAdmin(reversion.VersionAdmin):
         'dato',
     )
     list_display = readonly_fields
-    list_filter = ('sendadreso', 'ricevanto', 'partoprenanto',
-                   'retposhtajho', 'dato', 'temo',)
+    list_filter = ('sendadreso', 'ricevanto', 'retposhtajho', 'dato', 'temo',
+                   'partoprenanto')
     search_fields = ('temo', 'teksto')
     def has_add_permission(self, request): return False
     #def has_change_permission(self, request, obj=None): return False
@@ -227,7 +226,7 @@ class SenditaRetposhtajhoInline(admin.TabularInline):
     readonly_fields = SenditaRetposhtajhoAdmin.readonly_fields
     actions = None
     can_delete = False
-    
+
 class PartoprenantoAdmin(reversion.VersionAdmin):
     '''Unuopa partoprenanto en la kongreso'''
     fields = (
@@ -253,10 +252,12 @@ class PartoprenantoAdmin(reversion.VersionAdmin):
         ('chu_alvenis', 'chu_havasmanghkuponon', 'chu_havasnomshildon',)
     )
     readonly_fields = ('id', 'alighdato',)
-    list_display = ('persona_nomo', 'familia_nomo', 'loghlando', 'chambro')
+    list_display = ('persona_nomo', 'familia_nomo', 'sekso',
+                    'loghlando', 'chu_kontrolita', 'chambro')
     list_editable = ('loghlando', 'chambro')
     list_display_links = ('persona_nomo', 'familia_nomo')
-    list_filter = ('sekso', 'naskighdato', 'loghlando',
+    list_filter = ('chu_kontrolita',
+        'sekso', 'naskighdato', 'loghlando',
         'chu_bezonas_invitleteron', 'chu_invitletero_sendita',
         'chu_retalisto', 'chu_postkongresalisto', 'ekde', 'ghis',
         'interesighas_pri_antaukongreso', 'interesighas_pri_postkongreso',
@@ -265,7 +266,6 @@ class PartoprenantoAdmin(reversion.VersionAdmin):
         'chu_preferas_unuseksan_chambron', 'chu_malnoktemulo', 'chambro',
         'manghomendoj', 'manghotipo', 'antaupagos_ghis', 'pagmaniero',
         'chu_ueamembro',
-        'chu_kontrolita',
         'unua_konfirmilo_sendita', 'dua_konfirmilo_sendita',
         'alighdato', 'malalighdato',
         'chu_alvenis', 'chu_havasmanghkuponon', 'chu_havasnomshildon',)
