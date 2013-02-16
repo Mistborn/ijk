4e7c19c6b3e0ab0c7763740ea187d90583a459db
Adam Mesha <adam@mesha.org>
2013-02-16 19:22:52 +0200

    basic fabfile

---
+++ fabfile.py
@@ -0,0 +1,30 @@
+from fabric.api import *
+
+env.user = 'mesha'
+env.hosts = ['mesha.webfactional.com']
+
+def restart_live():
+    with cd('/home/mesha/webapps/ijk/apache2/bin'):
+        run('./restart')
+
+def restart_test():
+    with cd('/home/mesha/webapps/ijktest/apache2/bin'):
+        run('./restart')
+
+def update_test():
+    with cd('/home/mesha/webapps/ijktest/ijk'):
+        run('git pull')
+        run('./manage.py collectstatic')
+        run('./manage.py migrate alighi')
+    restart_test()
+
+def update_live():
+    with cd('/home/mesha/webapps/ijk/ijk'):
+        run('git pull')
+        run('./manage.py collectstatic')
+        run('./manage.py migrate alighi')
+    restart_live()
+
+def scss():
+    with lcd('/home/amesha/projects/ijk/ijk/static/css'):
+        local('sass -t expanded --update style.scss:style.css')
