12c747a7b6e573072a7554d6a3614e653a1d9bb4
Adam Mesha <adam@mesha.org>
2013-07-30 06:40:11 +0000

    Fix the alighi/ntoj redirect.

--- alighi/urls.py
+++ alighi/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 from django.core.urlresolvers import reverse
-
+from django.shortcuts import redirect
 
 urlpatterns = patterns('',
     url(r'^$', 'alighi.views.alighi', name='alighi'),
     url(r'^ntoj/', 
-        lambda request, url: redirect(reverse('alighintoj'))),
+        lambda *args: redirect(reverse('alighintoj'))),
     url(r'^gratulon/$', 'alighi.views.gratulon', name='gratulon'),
     url(r'^alighintoj/$', 'alighi.views.alighintoj', name='alighintoj'),
 )
