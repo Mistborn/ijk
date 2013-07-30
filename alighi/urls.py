1e7862dd544731e3e022a160f6f742466b077c51
Adam Mesha <adam@mesha.org>
2013-07-30 09:33:25 +0300

    Redirect from test alighintoj page.

--- alighi/urls.py
+++ alighi/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, url
+from django.core.urlresolvers import reverse
+
 
 urlpatterns = patterns('',
     url(r'^$', 'alighi.views.alighi', name='alighi'),
+    url(r'^ntoj/', 
+        lambda request, url: redirect(reverse('alighintoj'))),
     url(r'^gratulon/$', 'alighi.views.gratulon', name='gratulon'),
     url(r'^alighintoj/$', 'alighi.views.alighintoj', name='alighintoj'),
 )
